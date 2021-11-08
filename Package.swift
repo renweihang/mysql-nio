ee535ad7b59e177a9ac8ac4ed0a5f6750b1613b8
renwh <frank.ren@aliyun.com>
2021-11-08 14:20:26 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .library(name: "MySQLNIO", targets: ["MySQLNIO"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
+        .package(url: "https://github.com/renweihang/swift-crypto.git", "1.0.0" ..< "3.0.0"),
+        .package(url: "https://github.com/renweihang/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/renweihang/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/renweihang/swift-nio-ssl.git", from: "2.14.0"),
     ],
     targets: [
         .target(name: "MySQLNIO", dependencies: [
